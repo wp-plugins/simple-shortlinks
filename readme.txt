cdedb0babb2bfa3a391d15e03f8c7280de9be6c1
miqrogroove <miqrogroove@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-27 03:23:06 +0000

    Commit current stable v1.1
    git-svn-id: https://plugins.svn.wordpress.org/simple-shortlinks/trunk@187211 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,13 +3,15 @@ Contributors: miqrogroove
 Tags: shortlinks, short, links, url, tiny
 Requires at least: 2.5
 Tested up to: 2.9
-Stable tag: 1.0
+Stable tag: 1.1
 
-Use WordPress native ID forwarding for HTTP and HTML shortlinks.
+Automatically advertise shortlinks using WordPress native ID forwarding.
 
 == Description ==
 
 Advertises short URLs similar to post GUIDs using page headers.  These can be useful for micro-blogging, and allow you to use your own domain instead of a 3rd-party short URL service.
+
+A template tag enables you to display a human-readable link in addition to the automatically generated headers.
 
 == Installation ==
 
@@ -22,5 +24,39 @@ Deactivation removes everything except the files you uploaded.  There is no "uni
 
 == Changelog ==
 
+= 1.1 =
+* Added a template tag, released 26 December 2009
+
 = 1.0 =
 * First version, released 24 December 2009
+
+== Theme ==
+
+Here is a basic reference for the template functions provided by Simple Short Links.
+
+`
+/**
+ * Template Tag for Displaying the Short Link
+ *
+ * Must be called from inside "The Loop"
+ *
+ * Call like <?php the_shortlink(__('Shortlinkage FTW')); ?>
+ *
+ * @since 1.1
+ * @param string $text Optional The link text or HTML to be displayed.  Defaults to 'This is the short link.'
+ * @param string $title Optional The tooltip for the link.  Must be sanitized.  Defaults to the sanitized post title.
+ */
+function the_shortlink($text = '', $title = '');
+
+`
+
+If you use that template tag at all, you should also add this contingency to your theme's functions.php file:
+
+`
+if (!function_exists('the_shortlink')) {
+    function the_shortlink($a = '', $b = '') {
+        return; //Just define this function in case its plugin is ever missing.
+    }
+}
+
+`
--- shortlinks.php
+++ shortlinks.php
@@ -10,7 +10,7 @@
  * Author URI: http://www.miqrogroove.com/
  *
  * @author: Robert Chapin (miqrogroove)
- * @version: 1.0
+ * @version: 1.1
  * @copyright Copyright © 2009 by Robert Chapin
  * @license GPL
  *
@@ -27,9 +27,36 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+ 
+/* Plugin Bootup */
 
 add_action('wp', 'miqro_shortlink_http', 10, 0);
 add_action('wp_head', 'miqro_shortlink_html', 10, 0);
+
+
+/* Template Functions */
+
+/**
+ * Template Tag for Displaying the Short Link
+ *
+ * Must be called from inside "The Loop"
+ *
+ * Call like the_shortlink(__('Shortlinkage FTW'))
+ *
+ * @since 1.1
+ * @param string $text Optional The link text or HTML to be displayed.  Defaults to 'This is the short link.'
+ * @param string $title Optional The tooltip for the link.  Must be sanitized.  Defaults to the sanitized post title.
+ */
+function the_shortlink($text = '', $title = '') {
+    global $post;
+    if (strlen($text) == 0) $text = 'This is the short link.';
+    if (strlen($title) == 0) $title = the_title_attribute(array('echo' => FALSE));
+    $shortlink = miqro_get_the_shortlink($post->ID);
+    echo "<a rel='shortlink' href='$shortlink' title='$title'>$text</a>";
+}
+
+
+/* Plugin Functions */
 
 /**
  * Output a shortlink HTTP header.
@@ -65,8 +92,12 @@ function miqro_shortlink_html() {
  * Create a shortlink given an ID from the posts table.
  *
  * This is sometimes different from the GUID.
+ *
+ * @param int $pid ID number from the posts table.
+ * @return string The short link absolute URL.
  */
 function miqro_get_the_shortlink($pid) {
     $pid = (int) $pid;
     return user_trailingslashit(get_bloginfo('url', 'display'))."?p=$pid";
 }
+?>
